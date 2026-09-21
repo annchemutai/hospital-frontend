17df1a2a7489196546aa4e5129b2195ec2b2b1d7
Ann Chemutai <annchemutai@Anns-MacBook-Pro.local>
2026-09-21 09:37:24 +0300

    first commit

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+    <v-container max-width="100%">
+        <v-row>
+            <v-col>
+                <v-carousel cycle hide-delimiters>
+                    <v-carousel-item src="/images/carousel1.jpg" cover></v-carousel-item>
+                    <v-carousel-item src="/images/carousel2.jpg" cover></v-carousel-item>
+                    <v-carousel-item src="/images/carousel3.jpg" cover></v-carousel-item>
+                    <v-carousel-item src="/images/carousel4.jpg" cover></v-carousel-item>
+                </v-carousel>
+            </v-col>
+        </v-row>
+    </v-container>
+
+    <!-- About us -->
+     <v-container max-width="100%">
+        <v-row>
+            <v-col>
+                <v-card align="center">
+                    <v-card-title>Welcome to the Madaraka Hospital</v-card-title>
+                    <v-card-text class="text-body-large"> Welcome to The Madaraka Hospital, where your health, dignity, and peace of mind are our highest priorities. Rooted in the spirit of independence and community empowerment, our state-of-the-art medical facility delivers compassionate, world-class healthcare tailored to you and your family. From cutting-edge diagnostics and 24/7 emergency response to specialized maternal and critical care, our dedicated team of medical professionals combines clinical excellence with advanced technology.</v-card-text>
+                    <v-card-text class="text-body-large">At The Madaraka Hospital, we are committed to building a healthier, stronger future by putting patient-centered healing at the heart of everything we do.</v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+     </v-container>
+</template>
---
+++ src/components/ViewPatient.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>View Patient</h1>
+</template>
