a730f9b2ccc8d11ac09f8002e5752850d2d6ba66
Ann Chemutai <annchemutai@Anns-MacBook-Pro.local>
2026-09-21 11:06:22 +0300

    Landing Page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -28,4 +28,66 @@
             </v-col>
         </v-row>
      </v-container>
+
+     <!-- clinics -->
+     <v-container max-width="100%">
+        <v-row>
+            <v-col md="3">
+                <v-card width="300px" color="primary">
+                    <v-img src="/images/clinic1.jpg" cover></v-img>
+                    <v-card-title> Laboratory Services</v-card-title>
+                    <v-card-text>Some text about the lab</v-card-text>
+                    <v-btn class="ma-4">Learn More</v-btn>
+                </v-card>
+            </v-col>
+
+            <v-col md="3">
+                <v-card width="300px" color="primary">
+                    <v-img src="/images/clinic2.jpg" cover></v-img>
+                    <v-card-title> Maternity Services</v-card-title>
+                    <v-card-text>Some text about the maternity</v-card-text>
+                    <v-btn class="ma-4">Learn More</v-btn>
+                </v-card>
+            </v-col>
+            <v-col md="3">
+                <v-card width="300px" color="primary">
+                    <v-img src="/images/clinic3.jpg" cover></v-img>
+                    <v-card-title> Physiotherapy Services</v-card-title>
+                    <v-card-text>Some text about the Physiotherapy</v-card-text>
+                    <v-btn class="ma-4">Learn More</v-btn>
+                </v-card>
+            </v-col>
+            <v-col md="3">
+                <v-card width="300px" color="primary">
+                    <v-img src="/images/clinic4.jpg" cover></v-img>
+                    <v-card-title> Dental Services</v-card-title>
+                    <v-card-text>Some text about the Dental</v-card-text>
+                    <v-btn class="ma-4">Learn More</v-btn>
+                </v-card>
+            </v-col>
+        </v-row>
+     </v-container>
+
+     <!-- Footer -->
+      <v-container max-width="100%">
+        <v-row>
+            <v-col md="4">
+                <v-card>
+                    <v-card-title>Subscribe to our mailing list</v-card-title>
+                    <v-card-text>Get the latest news, health tips, and hospital updates delivered to your inbox.</v-card-text>
+                    <v-text-field v-model="inputValue" label="Email address" outlined class="ma-4">
+                        <template v-slot:append-inner>
+                        <v-btn color="primary" > Subscribe </v-btn>
+                        </template>
+                    </v-text-field>
+                    <div class="text-center">
+                        <p>Follow us on:</p>
+                        <v-icon icon="mdi-instagram"></v-icon>
+                        <v-icon icon="mdi-twitter"></v-icon>
+                        <v-icon icon="mdi-facebook"></v-icon>
+                    </div>
+                </v-card>
+            </v-col>
+        </v-row>
+      </v-container>
 </template>
